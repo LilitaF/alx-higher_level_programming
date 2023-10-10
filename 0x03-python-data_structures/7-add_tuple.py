3950488493fd61467cd06e300688ff0a1641be87
Lilita <lilitambete2911@gmail.com>
2023-10-10 13:19:05 -0700

    8-multiple_returns.py

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,17 +1,14 @@
 #!/usr/bin/python3
 
 def add_tuple(tuple_a=(), tuple_b=()):
-    #first two elements from each tuple
     a = tuple_a[0] if len(tuple_a) > 0 else 0
     b = tuple_a[1] if len(tuple_a) > 1 else 0
     c = tuple_b[0] if len(tuple_b) > 0 else 0
     d = tuple_b[1] if len(tuple_b) > 1 else 0
     
-    #adding for new elements
     e = a + c
     f = b + d
 
-    #create new tuple
     tuple_c = (e, f)
 
     return (tuple_c)
---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def multiple_returns(sentence):
+    sentence_len = len(sentence)
+    first_char = sentence[0]
+
+    tuple_return = (sentence_len, first_char)
+    return (tuple_return)
