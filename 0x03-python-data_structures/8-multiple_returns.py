4e399783882bacc83ac31ef659dffb180932b1ca
Lilita <lilitambete2911@gmail.com>
2023-10-10 13:32:05 -0700

    8-multiple_returns.py

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -4,9 +4,8 @@ def multiple_returns(sentence):
     if len(sentence) == 0:
         first_char = None
     else:
-        #if sentence is not empty
         first_char = sentence[0]
-        #get length
+
     sentence_len = len(sentence)
 
     tuple_return = (sentence_len, first_char)
