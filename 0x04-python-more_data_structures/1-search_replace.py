1652e3852dcd516ee173c2906edc761e51fdf999
Lilita <lilitambete2911@gmail.com>
2023-10-11 09:47:01 -0700

    1-search_replace.py

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def search_replace(my_list, search, replace):
+    new_my_list = []
+    for element in my_list:
+        if element == search:
+            new_my_list.append(replace)
+        else:
+            new_my_list.append(element)
+
+    return (new_my_list)
---
+++ 0x04-python-more_data_structures/README.md
@@ -0,0 +1,2 @@
+This directory is aimed at learning more about data structures in Python. 
+This time we learn about sets and dictionary.
