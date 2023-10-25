66951a18fb938ecb5ea409fa8cbffc8a656f9311
Lilita <lilitambete2911@gmail.com>
2023-10-25 13:05:02 -0700

    12-fizzbuzz.py

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -1,17 +1,3 @@
 #!/usr/bin/python3
 def pow(a, b):
-    # Base case: when exponent is 0, result is 1
-    if b == 0:
-        return 1
-
-    # If exponent is negative, compute reciprocal of the number raised to positive exponent
-    if b < 0:
-        a = 1 / a
-        b = abs(b)
-
-    result = 1
-    # Multiply the base 'a' 'b' times to calculate a^b
-    for _ in range(b):
-        result *= a
-
-    return (result)
+    return (a ** b)
