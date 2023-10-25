a571ca01541ea37d09a28a7bbc6242b95c28b069
Lilita <lilitambete2911@gmail.com>
2023-10-25 12:48:02 -0700

    101-square.py

---
+++ 0x06-python-classes/101-square.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+"""definde class square"""
+class Square:
+    """Respresent the square"""
+    def __init__(self, size=0, position=(0, 0)):
+        """initialis the square"""
+        self.size = size
+        self.position = position
+
+    @property
+    def size(self):
+        """get or set size"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """get or set size"""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    @property
+    def position(self):
+        """get or set position"""
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        """get or set position"""
+        if not isinstance(value, tuple) or len(value) != 2 or not all(isinstance(i, int) for i in value) or any(i < 0 for i in value):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
+
+    def area(self):
+        """get or set area"""
+        return self.__size ** 2
+
+    def my_print(self):
+        if self.__size == 0:
+            print()
+        else:
+            for _ in range(self.__position[1]):
+                print()
+            for _ in range(self.__size):
+                print(" " * self.__position[0] + "#" * self.__size)
+
