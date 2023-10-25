337457ca24ec3358c04f3e3dbf2a3d96d264c0ce
Lilita <lilitambete2911@gmail.com>
2023-10-25 15:56:59 -0700

    square doc fixed

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
+
 """Defines a Square"""
+
 class Square:
     """Represent a square"""
     def __init__(self, size):
--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,5 +1,7 @@
-#!/usr/bin/python3
+#!/usri/bin/python3
+
 """Define a class square"""
+
 class Square:
     """Represent the square"""
     def __init__(self, size=0):
