3d69f181c0c01f3f4df69e6990fc36415e11f758
Lilita <lilitambete2911@gmail.com>
2023-10-24 18:16:57 -0700

    1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Defines a Square"""
+class Square:
+    """Represent a square"""
+    def __init__(self, size):
+        """Initialise the square"""
+        self.__size = size
+
