fbdace10bfb2ed58142d537adba370d6f9bcc871
Lilita <lilitambete2911@gmail.com>
2023-10-30 16:32:03 -0700

    1-rectangle.py

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+
+"""Defining a class"""
+class Rectangle:
+    """Represent class: rectangle"""
+    def __init__(self, width=0, height=0):
+        """Initialise class: rectangle"""
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """Get width: width is private"""
+        return (self.__width)
+
+    @width.setter
+    def width(self, value):
+        """set the value of width"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if width < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Get value of height: height is private"""
+        return (self.__height)
+
+    @height.setter
+    def height(self, value):
+        """Set the value of height"""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if height < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
