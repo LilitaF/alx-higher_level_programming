ecf1784abf722475a6f72d994fbbb54cb50786ed
Lilita <lilitambete2911@gmail.com>
2023-11-14 06:33:04 -0800

    square.py

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+from models.rectangle import rectangle
+
+"""Define the class Square that inherits from rectangle"""
+class Square(Rectangle):
+    """Represent the class Square"""
+    def __init__(self, size, x=0, y=0, id=None):
+
